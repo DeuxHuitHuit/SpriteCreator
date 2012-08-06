2ef3ab6d246c32f6ca7fed9e842a43cb65af6673
Nicolas Brassard <nico@nitriques.com>
2012-08-06 11:31:44 -0400

    First working version

--- src/com.deuxhuithuit.SpriteCreator.Console/Main.vb
+++ src/com.deuxhuithuit.SpriteCreator.Console/Main.vb
@@ -1,9 +1,9 @@
 ﻿Imports System.Console
+Imports System.IO
 
 Module Main
 
-
-    Private targetFolder As String = "E:\Temp\sprite-test"
+    Private targetFolder As String = "E:\github\SpriteCreator\test\"
     Private file As String = "sprite.png"
     Private fileFilter As String = "*.png"
 
@@ -15,31 +15,54 @@ Module Main
         WriteLine("Taget Folder: {0}", targetFolder)
         WriteLine("Output File: {0}", file)
         WriteLine()
-        WriteLine()
-        Threading.Thread.Sleep(1000)
-        Write(" -> 3 -> ")
-        Threading.Thread.Sleep(1000)
-        Write("2 -> ")
-        Threading.Thread.Sleep(1000)
-        Write("1 -> ")
-        Threading.Thread.Sleep(1000)
-        WriteLine(" GO!")
-        WriteLine()
+        'WriteLine()
+        'Threading.Thread.Sleep(1000)
+        'Write(" -> 3 -> ")
+        'Threading.Thread.Sleep(1000)
+        'Write("2 -> ")
+        'Threading.Thread.Sleep(1000)
+        'Write("1 -> ")
+        'Threading.Thread.Sleep(1000)
+        'WriteLine(" GO!")
+        'WriteLine()
+
+        ' Assure we have a folder
+        AssureFolder()
 
         Dim start As Date = Now
+        Dim dirInfo As IO.DirectoryInfo = FileIO.FileSystem.GetDirectoryInfo(targetFolder)
 
-        Dim fileInfo As IO.DirectoryInfo = FileIO.FileSystem.GetDirectoryInfo(targetFolder)
+        If dirInfo IsNot Nothing AndAlso dirInfo.Exists Then
+            Dim sc As New SpriteCreator.Core.SpriteCreator(targetFolder, file, fileFilter)
+            Dim files As List(Of String) = sc.GetAllImageInFolder
 
-        If fileInfo IsNot Nothing AndAlso fileInfo.Exists Then
+            If files.Count < 1 Then
+                WriteLine("No file found. Can not continue.")
+            Else
+                For Each f As String In files
+                    WriteLine(f)
+                Next
+                WriteLine()
+                WriteLine("Creating sprite...")
+                sc.CreateSprite()
+                WriteLine("Sprite created successfully!")
+            End If
 
         Else
-            WriteLine("ERROR: Folder '{0}' does not exists. Can not continue.", fileInfo.FullName)
+            WriteLine("ERROR: Folder '{0}' does not exists. Can not continue.", dirInfo.FullName)
         End If
+
         WriteLine()
         WriteLine("Took {0:0.000} minutes", (Now - start).TotalMinutes)
         WriteLine()
         WriteLine("Hit <Enter> to exit...")
         ReadLine()
+    End Sub
+
+    Private Sub AssureFolder()
+        If String.IsNullOrWhiteSpace(targetFolder) Then
+            targetFolder = Directory.GetCurrentDirectory()
+        End If
     End Sub
 
     Private Sub parseArgs()
--- src/com.deuxhuithuit.SpriteCreator.Core/SpriteCreator.vb
+++ src/com.deuxhuithuit.SpriteCreator.Core/SpriteCreator.vb
@@ -1,3 +1,56 @@
-﻿Public Class SpriteCreator
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.Collections.ObjectModel
+
+Imports System.Drawing
+Imports System.Drawing.Imaging
+
+Public Class SpriteCreator
+
+    Private _targetFolder As String
+    Private _file As String
+    Private _fileFilter As String
+    Private _fileList As List(Of String)
+
+    Public Sub New(targetFolder As String, file As String, fileFilter As String)
+        _targetFolder = targetFolder
+        _file = file
+        _fileFilter = fileFilter
+    End Sub
+
+    Public Function GetAllImageInFolder() As List(Of String)
+        If _fileList Is Nothing Then
+            Dim roc As ReadOnlyCollection(Of String) = FileSystem.GetFiles(_targetFolder, FileIO.SearchOption.SearchTopLevelOnly, _fileFilter)
+            _fileList = roc.ToList
+        End If
+        Return _fileList
+    End Function
+
+    Public Function CreateSprite() As Boolean
+        Dim refImage As New Bitmap(_fileList(0), True)
+
+        Dim height = refImage.Height
+        Dim width = refImage.Width
+        Dim sprite As New Bitmap(width, height * _fileList.Count, refImage.PixelFormat)
+        Dim g As Graphics = Graphics.FromImage(sprite)
+        Dim counter As Integer = 0
+
+        ' Dispose our ref image
+        refImage.Dispose()
+
+        For Each f As String In _fileList
+            Dim bm = New Bitmap(_fileList(counter), True)
+            g.DrawImage(bm, 0, counter * height, width, height)
+            bm.Dispose()
+            counter += 1
+        Next
+
+        ' Dispose graphics
+        g.Dispose()
+
+        ' Save the image
+        sprite.Save(_targetFolder & _file)
+
+        Return True
+    End Function
 
 End Class
